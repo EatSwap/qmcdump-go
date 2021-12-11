b294e9a1381ebaba7a5c87eb88c01089ee2ef231
Lam Haoyin <root@eatswap.org>
2021-12-12 01:30:39 +0800

    add: basic dragging func

--- convert.go
+++ convert.go
@@ -4,25 +4,27 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
+	"time"
 )
 
 const (
 	BUFFER_SIZE = 8192
 )
 
-func ConvertFile(in, out string) (bool, error) {
+func ConvertFile(in, out string) (bool, int, error) {
 	// Trying to open input file
 	inFile, err := os.Open(in)
 
 	if err != nil {
-		return false, err
+		return false, -1, err
 	}
 
 	// Opening output file
 	outFile, err := os.OpenFile(out, os.O_WRONLY|os.O_CREATE, 0666)
 
 	if err != nil {
-		return false, err
+		return false, -1, err
 	}
 
 	// Creating buffer
@@ -33,7 +35,8 @@ func ConvertFile(in, out string) (bool, error) {
 	writer := bufio.NewWriter(outFile)
 	defer writer.Flush()
 
-	for offset := 0; ; {
+	offset := 0
+	for {
 		// Reading data from input file
 		n, err := reader.Read(buffer)
 
@@ -50,9 +53,30 @@ func ConvertFile(in, out string) (bool, error) {
 		_, err = writer.Write(data)
 		if err != nil {
 			fmt.Println("Warning: Unexpected error -> " + err.Error())
-			return false, err
+			return false, -1, err
 		}
 	}
 
-	return true, nil
+	return true, offset, nil
+}
+
+func Convert(in string) (bool, int, error) {
+	fmt.Printf("Converting %s ...\n", in)
+	start := time.Now()
+	defer func() {
+		fmt.Println("Time elapsed: ", time.Since(start))
+	}()
+	if strings.HasSuffix(in, ".flac") || strings.HasSuffix(in, ".mp3") {
+		fmt.Printf("%s has a suffix of .flac / .mp3, assuming already converted.\n", in)
+		return true, 0, nil
+	} else if strings.HasSuffix(in, ".qmcflac") {
+		return ConvertFile(in, strings.TrimSuffix(in, ".qmcflac")+".flac")
+	} else if strings.HasSuffix(in, ".qmc0") {
+		return ConvertFile(in, strings.TrimSuffix(in, ".qmc0")+".mp3")
+	} else if strings.HasSuffix(in, ".qmc3") {
+		return ConvertFile(in, strings.TrimSuffix(in, ".qmc3")+".mp3")
+	} else {
+		fmt.Println("Unrecognised file suffix in ", in, ", ignored.")
+		return true, 0, nil
+	}
 }
--- main.go
+++ main.go
@@ -1,6 +1,51 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
 func main() {
-	ConvertFile("test.qmcflac", "test.flac")
-	// Nothing yet.
+	args := os.Args
+	if len(args) == 1 {
+		fmt.Printf("Usage: %s <FILE/FOLDER>\n", args[0])
+		os.Exit(1)
+	}
+	stat, err := os.Stat(args[1])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	if stat.IsDir() {
+		filepath.Walk(args[1], func(path string, info os.FileInfo, err error) error {
+			if err != nil || info.IsDir() {
+				fmt.Println(err)
+				return err
+			}
+			ok, _, err := Convert(path)
+			if err != nil {
+				fmt.Println("Conversion failed. Message:")
+				fmt.Println(err)
+			} else if !ok {
+				fmt.Println("Conversion failed with unknown reason.")
+			} else {
+				fmt.Println("Conversion completed successfully.")
+			}
+			fmt.Println("")
+			return nil
+		})
+	} else {
+		ok, _, err := Convert(args[1])
+		if err != nil {
+			fmt.Println("Conversion failed. Message:")
+			fmt.Println(err)
+		} else if !ok {
+			fmt.Println("Conversion failed with unknown reason.")
+		} else {
+			fmt.Println("Conversion completed successfully.")
+		}
+	}
+	fmt.Println("Press <Enter> to exit...")
+	fmt.Scanln()
 }
